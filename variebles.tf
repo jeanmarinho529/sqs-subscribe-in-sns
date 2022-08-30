9d9f4621aac42ac9f65f874ceee26002cab7517d
marinho <jeanmarinho529@gmail.com>
2022-08-30 13:19:19 -0300

    add description in variebles

--- variebles.tf
+++ variebles.tf
@@ -1,9 +1,11 @@
 variable "topic_queue" {
   type = map(string)
+  description = "value"
 }
 
 variable "team" {
   type = string
+  description = "name of the team that owns the resource"
 }
 
 variable "prefixes" {
@@ -12,6 +14,8 @@ variable "prefixes" {
     queue     = string
     queue_dlq = string
   })
+
+  description = "prefixes to create the resource name"
 
   default = {
     queue     = "prefix_queue"
@@ -27,6 +31,8 @@ variable "use_prefixes" {
     queue_dlq = bool
   })
 
+  description = "use prefix in resource name"
+
   default = {
     queue     = true
     queue_dlq = true
@@ -36,11 +42,13 @@ variable "use_prefixes" {
 
 variable "tags" {
   type = map(string)
+  description = "tags"
   default = {}
 }
 
 variable "topic_tags" {
   type = map(string)
+  description = "topic tags"
   default = {
     product = "sns"
   }
@@ -48,6 +56,7 @@ variable "topic_tags" {
 
 variable "queue_tags" {
   type = map(string)
+  description = "queue tags"
   default = {
     product = "sqs"
   }
@@ -55,6 +64,7 @@ variable "queue_tags" {
 
 variable "queue_actions" {
   type = list(string)
+  description = "queue actions"
   default = [
     "sqs:SendMessage"
   ]
@@ -62,16 +72,19 @@ variable "queue_actions" {
 
 variable "queue_version" {
   type = string
+  description = "queue version"
   default = "2008-10-17"
 }
 
 variable "topic_version" {
   type = string
+  description = "topic version"
   default = "2008-10-17"
 }
 
 variable "topic_actions" {
   type = list(string)
+  description = "topic actions"
   default = [
       "SNS:Subscribe",
       "SNS:SetTopicAttributes",
@@ -87,41 +100,12 @@ variable "topic_actions" {
 
 variable "queue_police_settings" {
   type = map(any)
+  description = "queue police settings"
   default = {}
 }
 
 variable "topic_police_settings" {
   type = map(any)
+  description = "topic police settings"
   default = {}
 }
-
-# variable "queue_settings" {
-#   type        = object({
-#     name = string
-#     nome = optional(string)
-#   })
-#   description = "subscribe protocol"
-# }
-
-# variable "queues_topics" {
-#   type = list(map(string))
-# }
-
-# variable "sns" {
-#   type = string
-# }
-
-# variable "sqs" {
-#   type = list
-# }
-
-# variable "topic_queue" {
-#   type = object({
-#     topic = string
-#     queue = string
-#   })
-# }
-
-# variable "index" {
-#   type = number
-# }
